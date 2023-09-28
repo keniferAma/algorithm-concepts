bc70382072c2ba53a8d80a6ed54a46edcb2e7715
kenifer <139505395+keniferAma@users.noreply.github.com>
2023-09-28 12:24:05 -0500

    practices-adds merge sort(no completed) copy exercise

--- insertion merge/practices.py
+++ insertion merge/practices.py
@@ -25,3 +25,40 @@ def insertio_sort_repaso(array):
 
 array = [5, 7, 9, 3, 1, 3, 7]
 print(insertio_sort_repaso(array=array))
+
+
+
+# Ahora vamos a tratar de replicar el merge sort:
+
+A1 = [9, 7, 5, 2]
+A2 = [7, 5, 3, 1]
+
+def merge(array_1, array_2):
+    result = [9, 7, 5, 5, 7, 2, 3, 1]
+    i = 0
+    j = 0
+    k = 0
+
+    while i < len(array_1) and j < len(array_2):
+        if array_1[i] > array_2[j]:
+            result[k] = array_1[i]
+            i += 1
+
+        else:
+            result[k] = array_2[j]
+            j += 1
+
+        k += 1
+    while i < len(array_1):
+        result[k] = array_1[i]
+        i += 1
+        k += 1
+    
+    while j < len(array_2):
+        result[k] = array_2[j]
+        j += 1
+        k += 1
+
+    return result
+
+print(merge(array_1=A1, array_2=A2))
